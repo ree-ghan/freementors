baabbdbccd6aaf548db47bdd945ed35bcab428f6
ree-ghan <137789043+ree-ghan@users.noreply.github.com>
2023-08-17 11:27:54 +0300

    formatted the files

--- UI/js/client.js
+++ UI/js/client.js
@@ -1,25 +1,25 @@
-let andela = document.querySelector('.andela');
-let grid = document.querySelector('.grid');
-let mentors = document.querySelector('.mentors');
-let mentor = document.querySelector('.mentor');
-let page = document.querySelector('.page');
+let andela = document.querySelector(".andela");
+let grid = document.querySelector(".grid");
+let mentors = document.querySelector(".mentors");
+let mentor = document.querySelector(".mentor");
+let page = document.querySelector(".page");
 
 mentors.addEventListener("click", function (e) {
-  if (e.target.matches('.mentors')) {
-    andela.classList.add('hidden');
+  if (e.target.matches(".mentors")) {
+    andela.classList.add("hidden");
   }
 });
 
 mentors.addEventListener("click", function (e) {
-  if (e.target.matches('.mentors')) {
+  if (e.target.matches(".mentors")) {
     // page.removeChild(grid);
-    grid.classList.add('hidden');
+    grid.classList.add("hidden");
   }
 });
 
 mentors.addEventListener("click", function (e) {
-  if (e.target.matches('.mentors')) {
-    mentor.setAttribute('style', 'z-index: 0;');
-    mentor.classList.remove('hidden');
+  if (e.target.matches(".mentors")) {
+    mentor.setAttribute("style", "z-index: 0;");
+    mentor.classList.remove("hidden");
   }
 });
